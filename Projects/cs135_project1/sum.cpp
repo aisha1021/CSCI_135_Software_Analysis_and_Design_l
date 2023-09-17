c724b07adc30bdcfdb03707cba6f10c1c390472f
Aisha <128254669+aisha1021@users.noreply.github.com>
2023-09-17 16:05:53 -0400

    Add files via upload

---
+++ Projects/cs135_project1/sum.cpp
@@ -0,0 +1,21 @@
+// Name: Aisha Malik
+// Course: CSCI 135
+// Instructor: Tong Yi
+// Date: 09/03/2023
+// Assignment: Task A
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    int sum = 0;
+
+    while (cin >> num) {
+        sum += num;
+    }
+
+    cout << sum << endl;
+
+    return 0;
+}
