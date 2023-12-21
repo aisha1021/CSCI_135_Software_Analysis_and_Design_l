91f961259c46268c23de6eb78a48f6c55abf6507
Aisha <128254669+aisha1021@users.noreply.github.com>
2023-12-20 22:10:27 -0500

    Update lab-quiz-10.cpp

--- Labs/Quizzes/lab-quiz-10.cpp
+++ Labs/Quizzes/lab-quiz-10.cpp
@@ -18,8 +18,6 @@ Date is defined in your program
 #include <iostream>
 using namespace std;
 
-string formatDate(Date * d);
-
 class Date {
 public:
     int month;
@@ -27,42 +25,13 @@ public:
     int year;
 };
 
-int main() {
-    Date midterm1 = Date {10, 17, 2022};
-    cout << formatDate(&midterm1) << endl;
-
-    Date christmas = Date {12, 25, 2022};
-    cout << formatDate(&christmas) << endl;
+void printDate(Date *input) {
+    cout << input->month << "/" << input->day << "/" << input->year;
 }
 
-string formatDate(Date * d) {
-    string monthName;
-
-    if(d -> month == 1) {
-        monthName = "Jan";
-    } else if(d -> month == 2) {
-        monthName = "Feb";
-    } else if(d -> month == 3) {
-        monthName = "Mar";
-    } else if(d -> month == 4) {
-        monthName = "Apr";
-    } else if(d -> month == 5) {
-        monthName = "May";
-    } else if(d -> month == 6) {
-        monthName = "June";
-    } else if(d -> month == 7) {
-        monthName = "July";
-    } else if(d -> month == 8) {
-        monthName = "Aug";
-    } else if(d -> month == 9) {
-        monthName = "Sep";
-    } else if(d -> month == 10) {
-        monthName = "Oct";
-    } else if(d -> month == 11) {
-        monthName = "Nov";
-    } else if(d -> month == 12) {
-        monthName = "Dec";
-    } 
-
-    return monthName + " " + to_string(d -> day) + ", " + to_string(d -> year);
+int main() {
+    Date d = {10, 21, 2023};
+    printDate(&d);
+    cout << endl;
+    return 0;
 }
